224ea27b4f46f947bf7621f8fe60d14691600ee1
nnZoey <nguyentramzoey@gmail.com>
2022-04-27 14:48:20 +0700

    update table

--- src/main/java/vn/leap/intranet/repository/EmployeeRepository.java
+++ src/main/java/vn/leap/intranet/repository/EmployeeRepository.java
@@ -13,4 +13,7 @@ import vn.leap.intranet.domain.Employee;
 public interface EmployeeRepository extends JpaRepository<Employee, Long>, JpaSpecificationExecutor<Employee> {
     @Query("SELECT e FROM Employee e, User u where e.userId = u.id and u.id = ?1")
     Optional<Employee> findByUserId(Long id);
+
+    @Query("update Employee e set e.image = ?2, e.bankAccount = ?3, e.bankCode = ?4 where e.id = ?1")
+    void updateUserInformation(Long id, byte[] image, String bankAcc, String bankCo);
 }
